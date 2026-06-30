cfa64f5bc92cbd2555a8c72795b8fdf6a241319a
Aarya Suthar <acsuthar2006@gmail.com>
2026-06-30 08:39:30 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] arr, int k) {
+        int n = arr.length;
+
+        int left = 0;
+
+        for (int right = 1 ; right < n ; right++) {
+            if (right - left <= k && arr[right] == arr[left]) {
+                // System.out.println("LEFT = " + left + " --- RIGHT = " + right);
+                return true;
+
+            } else if (right - left <= k) {
+                right++;
+
+            } else {
+                left = right;
+            }
+        }
+
+        return false;
+    }
+}
