08cd84bbf6dc680f73c03ad4d6a7c23194d52904
Aarya Suthar <acsuthar2006@gmail.com>
2026-07-17 11:48:41 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+        left, right = min(piles), max(piles)
+
+        def isValid(piles, mid, h):
+            totalhours = 0
+            for pile in piles:
+                totalhours += math.floor(pile / mid)
+                if totalhours > h:
+                    return False
+            return True
+        
+        ans = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isValid(piles, mid, h):
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return ans
